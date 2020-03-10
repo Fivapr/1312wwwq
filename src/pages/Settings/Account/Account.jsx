f4b251787548038ab79b0642643618dea097d0df
intensifies <aintensifies@gmail.com>
2020-03-10 15:10:54 +0700

    asd

--- src/pages/Settings/Account/Account.jsx
+++ src/pages/Settings/Account/Account.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
-import Register from './Register';
-import Login from './Login';
-import ForgotPassword from './ForgotPassword';
-import ActivateCode from './ActivateCode';
-import ResetPassword from './ResetPassword';
-import Logout from './Logout';
+import Register from './Register'
+import Login from './Login'
+import ForgotPassword from './ForgotPassword'
+import ActivateCode from './ActivateCode'
+import ResetPassword from './ResetPassword'
+import Referral from '../Referral'
 
 const Pages = ({ match, isLoggedIn }) => {
-  const [email, setEmailForActivateCode] = useState('');
-  const [code, setCode] = useState('');
+  const [email, setEmailForActivateCode] = useState('')
+  const [code, setCode] = useState('')
 
   if (isLoggedIn) {
-    return <Logout />;
+    return <Referral />
   }
 
   return (
@@ -21,25 +21,39 @@ const Pages = ({ match, isLoggedIn }) => {
       <Route
         path={`${match.url}/register/`}
         render={() => (
-          <Register url={match.url} setEmailForActivateCode={setEmailForActivateCode} />
+          <Register
+            url={match.url}
+            setEmailForActivateCode={setEmailForActivateCode}
+          />
         )}
       />
       <Route
         path={`${match.url}/login/`}
         component={() => (
-          <Login url={match.url} setEmailForActivateCode={setEmailForActivateCode} />
+          <Login
+            url={match.url}
+            setEmailForActivateCode={setEmailForActivateCode}
+          />
         )}
       />
       <Route
         path={`${match.url}/forgot/`}
         render={() => (
-          <ForgotPassword url={match.url} setEmailForActivateCode={setEmailForActivateCode} />
+          <ForgotPassword
+            url={match.url}
+            setEmailForActivateCode={setEmailForActivateCode}
+          />
         )}
       />
       <Route
         path={`${match.url}/verify/`}
         render={() => (
-          <ActivateCode url={match.url} email={email} setVerifyCode={setCode} pathToPush="login" />
+          <ActivateCode
+            url={match.url}
+            email={email}
+            setVerifyCode={setCode}
+            pathToPush="login"
+          />
         )}
       />
       <Route
@@ -56,14 +70,16 @@ const Pages = ({ match, isLoggedIn }) => {
 
       <Route
         path={`${match.url}/resetPassword/`}
-        render={() => <ResetPassword url={match.url} email={email} code={code} />}
+        render={() => (
+          <ResetPassword url={match.url} email={email} code={code} />
+        )}
       />
       <Route
         path={`${match.url}`}
         render={() => <Redirect to={{ pathname: `${match.url}/login` }} />}
       />
     </Switch>
-  );
-};
+  )
+}
 
-export default Pages;
+export default Pages
--- src/pages/Settings/Referral/Referral.jsx
+++ src/pages/Settings/Referral/Referral.jsx
@@ -1,43 +1,55 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import React, { Fragment, useState, useEffect } from 'react'
+import { withStyles } from '@material-ui/core/styles'
 
-import { api } from '../../../loggerApi';
-import styles from './styles.js';
-import { DialogTitle, TableCell, TableRow, Table, TableBody, TextField } from '@material-ui/core';
+import { api } from '../../../loggerApi'
+import styles from './styles.js'
+import {
+  DialogTitle,
+  TableCell,
+  TableRow,
+  Table,
+  TableBody,
+  TextField
+} from '@material-ui/core'
 
 const Referral = ({ classes, user }) => {
   const [referrals, setReferrals] = useState({
     percents: { '1': 0.3, '2': 0.2, '3': 0.1 },
-    referrals: { '1': 0, '2': 0, '3': 0 },
-  });
+    referrals: { '1': 0, '2': 0, '3': 0 }
+  })
   useEffect(() => {
     const requestReferrals = async () => {
       try {
-        const response = await api.requestReferrals();
-        console.log('TCL: response', response);
-        setReferrals(response);
+        const response = await api.requestReferrals()
+        console.log('TCL: response', response)
+        setReferrals(response)
       } catch (e) {
-        console.log('TCL: Referral -> e', e);
+        console.log('TCL: Referral -> e', e)
       }
-    };
+    }
 
-    requestReferrals();
-  }, []);
+    requestReferrals()
+  }, [])
 
   return (
     <Fragment>
       <DialogTitle>My referrals</DialogTitle>
 
       <div className={classes.text}>
-        Onexbit has upgraded our Referral Program! Invite your friends to register and trade inside
-        our terminal, just share the code below and you will get part of your referral's income
-        after they buing subscriptions to our services.
+        Onexbit has upgraded our Referral Program! Invite your friends to
+        register and trade inside our terminal, just share the code below and
+        you will get part of your referral's income after they buing
+        subscriptions to our services.
       </div>
 
-      <TextField value={user.referralCode} className={classes.refCode} />
+      <TextField
+        value={`https://account.oneexbit.com/?refCode=${user.referralCode}`}
+        className={classes.refCode}
+      />
 
       <div className={classes.text}>
-        For each registered referral you will have these percentage of his deposits:
+        For each registered referral you will have these percentage of his
+        deposits:
       </div>
 
       <div className={classes.flex}>
@@ -80,10 +92,12 @@ const Referral = ({ classes, user }) => {
           </div>
         </>
       ) : (
-        <div className={classes.text}>Currently you don't have anyone invited.</div>
+        <div className={classes.text}>
+          Currently you don't have anyone invited.
+        </div>
       )}
     </Fragment>
-  );
-};
+  )
+}
 
-export default withStyles(styles)(Referral);
+export default withStyles(styles)(Referral)
